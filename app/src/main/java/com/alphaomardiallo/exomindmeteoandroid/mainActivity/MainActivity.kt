7548721ddafb5537c24d02b2aa794fa72d1c2ab6
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-09-06 09:34:01 +0200

    Initial commit - gradle setup done - hilt setup done - nav implemented

---
+++ app/src/main/java/com/alphaomardiallo/exomindmeteoandroid/mainActivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alphaomardiallo.exomindmeteoandroid.mainActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alphaomardiallo.exomindmeteoandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
